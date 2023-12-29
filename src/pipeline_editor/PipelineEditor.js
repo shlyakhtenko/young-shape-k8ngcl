84573509103e225fb6a774d4f426a41deb049b6e
Shlyakhtenko <90537026+shlyakhtenko@users.noreply.github.com>
2023-12-29 08:28:23 +0000

    Update PipelineEditor.js

--- src/pipeline_editor/PipelineEditor.js
+++ src/pipeline_editor/PipelineEditor.js
@@ -10,7 +10,7 @@ import {
 
 import get_pipeline_data from "./available_pipelines.js";
 import Datacolumn from "./Datacolumn.js";
-import get_data_sources from "./available_queries.js";
+
 //import Select from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -19,289 +19,299 @@ import { useParams } from "react-router-dom";
 
 export default function PipelineEditor(props) {
   let target_pipeline = null;
-  let pipeline_data = null;
+
   const token = useContext(LoginContext);
+
+  const [pipeline_data, setPipelineData] = useState(null);
+  const [pipelineName, setPipelineName] = useState(target_pipeline); // pipelineID
+  const [pipeline, setPipeline] = useState(target_pipeline);
+  const [data_sources, set_data_sources] = useState([]);
+  const [data_source, setDataSource] = useState(null);
+
+  const [inputs, setInputs] = useState([]);
+  const [query, setQuery] = useState({});
+  const [wips, setWIPS] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  const [outputs, setOutputs] = useState([
+    // compute_outputs(query, wips, "output", "Output Column"),
+  ]);
+  const [selectOptions, setSelectOptions] = useState([]);
 
   if (props.pipeline != "new") {
     let params = useParams();
     target_pipeline = params.pipelineName;
-    pipeline_data = get_pipeline_data().find((p) => p.name == target_pipeline);
+    setInputs(pipeline_data.inputs);
+    setWIPS(pipeline_data.wips);
+    setOutputs(pipeline_data.outputs);
+  } else {
+    target_pipeline = "Pipeline";
   }
 
-  if (pipeline_data == null) {
-    target_pipeline = "Pipeline";
-
-    var [pipelineName, setPipelineName] = useState(target_pipeline); // pipelineID
-    var [pipeline, setPipeline] = useState(target_pipeline);
-
-    var data_sources = get_data_sources();
-    var [data_source, setDataSource] = useState(data_sources[0]);
-    var [query, setQuery] = useState(data_source.available_queries[0]);
-    var [inputs, setInputs] = useState(compute_inputs(query));
-    //const [wips, setWIPS] = useState([compute_wips(query, 'wip1','Work in Progress')]);
-    var [wips, setWIPS] = useState([]);
-    var [outputs, setOutputs] = useState([
-      // compute_outputs(query, wips, "output", "Output Column"),
-    ]);
-
-    var [selectOptions, setSelectOptions] = useState(
-      data_source.available_queries.map((q) => {
-        return { value: q.name, label: q.caption };
-      }),
+  const load_query_data = () => {
+    const url = "https://docs.ipam.ucla.edu/cocytus/get_data_sources.php";
+    const headers = {};
+    fetch(url, { mode: "cors", headers: headers, method: "GET" }).then(
+      (response) => {
+        response.json().then((data) => {
+          set_data_sources(data);
+          setDataSource(
+            data_sources.find((x) => x.name == pipeline_data.data_source),
+          );
+          setQuery(
+            data_source.available_queries.find(
+              (x) => x.name == pipeline_data.inputs[0].query,
+            ),
+          );
+          setSelectOptions(
+            data_source.available_queries.map((q) => {
+              return { value: q.name, label: q.caption };
+            }),
+          );
+          setLoaded(true);
+        });
+      },
     );
-  } else {
-    [pipelineName, setPipelineName] = useState(target_pipeline); // pipelineID
-    [pipeline, setPipeline] = useState(pipeline_data.caption);
-
-    data_sources = get_data_sources();
-    [data_source, setDataSource] = useState(
-      data_sources.find((x) => x.name == pipeline_data.data_source),
-    );
-    [query, setQuery] = useState(
-      data_source.available_queries.find(
-        (x) => x.name == pipeline_data.inputs[0].query,
-      ),
-    );
-    [inputs, setInputs] = useState(pipeline_data.inputs);
-    //const [wips, setWIPS] = useState([compute_wips(query, 'wip1','Work in Progress')]);
-    [wips, setWIPS] = useState(pipeline_data.wips);
-    [outputs, setOutputs] = useState(pipeline_data.outputs);
-
-    [selectOptions, setSelectOptions] = useState(
-      data_source.available_queries.map((q) => {
-        return { value: q.name, label: q.caption };
-      }),
-    );
+  };
+  if (!loaded) {
+    load_query_data();
   }
 
   return (
     <div className="App">
       <h1>Pipeline Editor</h1>
-      <label className="pipelineName">
-        Pipeline name:
-        <Form.Control
-          type="text"
-          value={pipeline}
-          onChange={(e) => {
-            setPipeline(e.target.value);
-            if (pipelineName == "Pipeline") {
-              setPipelineName(e.target.value.split(" ").join("_"));
-            }
-          }}
-        />
-      </label>
-      <div className="preamble">
-        <label>
-          Datasource:
-          <Form.Select
-            className="data_selector"
-            defaultValue={data_source.name}
-            onChange={(ev) => {
-              let val = ev.target.value;
-              let new_data_source = data_sources.filter((ds) => {
-                return ds.name == val;
-              })[0];
-              setDataSource(new_data_source);
-              setInputs(compute_inputs(new_data_source.available_queries[0]));
-              setWIPS([]);
-              setOutputs([]);
-              setSelectOptions(
-                new_data_source.available_queries.map((q) => {
-                  return { value: q.name, label: q.caption };
-                }),
-              );
-            }}
-          >
-            {data_sources.map((ds) => {
-              return (
-                <option key={ds.name} value={ds.name}>
-                  {ds.caption}
-                </option>
-              );
-            })}
-          </Form.Select>
-        </label>
-        <label>
-          Query:
-          <Form.Select
-            className="data_selector"
-            //value={selectOptions[0]}
-            defaultValue={
-              //(pipeline_data.inputs[0].query)
-              query.name
-              /*label: data_source.available_queries.find(
-                (q) => q.name == pipeline_data.inputs[0].query,
-              ).caption,*/
-            }
-            onChange={(e) => {
-              let val = e.target.value;
-              let newquery = data_source.available_queries.filter((qn) => {
-                return qn.name == val;
-              })[0];
-              setQuery(newquery);
-              setInputs(compute_inputs(newquery));
-              setWIPS([]);
-              setOutputs([
-                //compute_outputs(newquery, [], "output", "Output Column"),
-              ]);
-            }}
-          >
-            {console.log(query)}
-            {selectOptions.map((q) => {
-              return (
-                <option key={q.value} value={q.value}>
-                  {q.label}
-                </option>
-              );
-            })}
-          </Form.Select>
-        </label>
-      </div>
-      <table className="columntable">
-        <thead>
-          <tr>
-            <th>
-              <h2>Inputs</h2>
-              <h4>New cards appear here</h4>
-            </th>
-            <th>
-              <h2>
-                To-Do&apos;s{" "}
-                <Button
-                  onClick={() => {
-                    setWIPS([
-                      ...wips,
-                      compute_wips(query, "wip" + wips.length + 1, ""),
-                    ]);
-                  }}
-                >
-                  Add Column
-                </Button>
-              </h2>
-              <h4>Cards you are working on appear here</h4>
-            </th>
-            <th>
-              <h2>
-                Done{" "}
-                <Button
-                  onClick={() => {
-                    setOutputs([
-                      ...outputs,
-                      compute_outputs(
-                        query,
-                        wips,
-                        "outputs" + outputs.length + 1,
-                        "",
-                      ),
-                    ]);
-                  }}
-                >
-                  Add Column
-                </Button>
-              </h2>
-              <h4>All cards should end up here</h4>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <div className="columnset">
-                <Datacolumn
-                  column_data={inputs[0]}
-                  siblings={inputs}
-                  setter={setInputs}
-                  key="inputs"
-                  use_field={false}
-                  edit_field={false}
-                />
-              </div>
-            </td>
-            <td>
-              <div className="columnset">
-                {wips.map((w) => {
+      {loaded ? (
+        <>
+          <label className="pipelineName">
+            Pipeline name:
+            <Form.Control
+              type="text"
+              value={pipeline}
+              onChange={(e) => {
+                setPipeline(e.target.value);
+                setPipelineName(e.target.value.split(" ").join("_"));
+              }}
+            />
+          </label>
+          <div className="preamble">
+            <label>
+              Datasource:
+              <Form.Select
+                className="data_selector"
+                defaultValue={data_source.name}
+                onChange={(ev) => {
+                  let val = ev.target.value;
+                  let new_data_source = data_sources.filter((ds) => {
+                    return ds.name == val;
+                  })[0];
+                  setDataSource(new_data_source);
+                  setInputs(
+                    compute_inputs(new_data_source.available_queries[0]),
+                  );
+                  setWIPS([]);
+                  setOutputs([]);
+                  setSelectOptions(
+                    new_data_source.available_queries.map((q) => {
+                      return { value: q.name, label: q.caption };
+                    }),
+                  );
+                }}
+              >
+                {data_sources.map((ds) => {
                   return (
-                    <Datacolumn
-                      column_data={{
-                        ...w,
-                        fields: w.fields,
-                      }}
-                      siblings={wips}
-                      setter={setWIPS}
-                      remove_button
-                      key={w.name}
-                      output_setter={setOutputs}
-                      use_field={true}
-                      edit_field={true}
-                    />
+                    <option key={ds.name} value={ds.name}>
+                      {ds.caption}
+                    </option>
                   );
                 })}
-              </div>
-            </td>
-            <td>
-              <div className="columnset">
-                {outputs.map((o) => {
+              </Form.Select>
+            </label>
+            <label>
+              Query:
+              <Form.Select
+                className="data_selector"
+                //value={selectOptions[0]}
+                defaultValue={
+                  //(pipeline_data.inputs[0].query)
+                  query.name
+                  /*label: data_source.available_queries.find(
+                (q) => q.name == pipeline_data.inputs[0].query,
+              ).caption,*/
+                }
+                onChange={(e) => {
+                  let val = e.target.value;
+                  let newquery = data_source.available_queries.filter((qn) => {
+                    return qn.name == val;
+                  })[0];
+                  setQuery(newquery);
+                  setInputs(compute_inputs(newquery));
+                  setWIPS([]);
+                  setOutputs([
+                    //compute_outputs(newquery, [], "output", "Output Column"),
+                  ]);
+                }}
+              >
+                {console.log(query)}
+                {selectOptions.map((q) => {
                   return (
+                    <option key={q.value} value={q.value}>
+                      {q.label}
+                    </option>
+                  );
+                })}
+              </Form.Select>
+            </label>
+          </div>
+          <table className="columntable">
+            <thead>
+              <tr>
+                <th>
+                  <h2>Inputs</h2>
+                  <h4>New cards appear here</h4>
+                </th>
+                <th>
+                  <h2>
+                    To-Do&apos;s{" "}
+                    <Button
+                      onClick={() => {
+                        setWIPS([
+                          ...wips,
+                          compute_wips(query, "wip" + wips.length + 1, ""),
+                        ]);
+                      }}
+                    >
+                      Add Column
+                    </Button>
+                  </h2>
+                  <h4>Cards you are working on appear here</h4>
+                </th>
+                <th>
+                  <h2>
+                    Done{" "}
+                    <Button
+                      onClick={() => {
+                        setOutputs([
+                          ...outputs,
+                          compute_outputs(
+                            query,
+                            wips,
+                            "outputs" + outputs.length + 1,
+                            "",
+                          ),
+                        ]);
+                      }}
+                    >
+                      Add Column
+                    </Button>
+                  </h2>
+                  <h4>All cards should end up here</h4>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="columnset">
                     <Datacolumn
-                      column_data={o}
-                      siblings={outputs}
-                      remove_button
-                      //keep_column={"output"}
-                      setter={setOutputs}
-                      key={o.name}
+                      column_data={inputs[0]}
+                      siblings={inputs}
+                      setter={setInputs}
+                      key="inputs"
                       use_field={false}
                       edit_field={false}
                     />
-                  );
-                })}
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <hr />
-      <Button
-        onClick={() => {
-          let save_data = {
-            data_source: data_source.name,
-            query: query.name,
-            name: pipelineName,
-            caption: pipeline,
-            inputs: inputs,
-            wips: wips,
-            outputs: outputs,
-          };
-          const headers = {
-            authorization: "Basic " + token,
-            "Content-Type": "application/json",
-          };
-          const url = "https://docs.ipam.ucla.edu/cocytus/save_pipeline.php";
-          const body = JSON.stringify(save_data);
+                  </div>
+                </td>
+                <td>
+                  <div className="columnset">
+                    {wips.map((w) => {
+                      return (
+                        <Datacolumn
+                          column_data={{
+                            ...w,
+                            fields: w.fields,
+                          }}
+                          siblings={wips}
+                          setter={setWIPS}
+                          remove_button
+                          key={w.name}
+                          output_setter={setOutputs}
+                          use_field={true}
+                          edit_field={true}
+                        />
+                      );
+                    })}
+                  </div>
+                </td>
+                <td>
+                  <div className="columnset">
+                    {outputs.map((o) => {
+                      return (
+                        <Datacolumn
+                          column_data={o}
+                          siblings={outputs}
+                          remove_button
+                          //keep_column={"output"}
+                          setter={setOutputs}
+                          key={o.name}
+                          use_field={false}
+                          edit_field={false}
+                        />
+                      );
+                    })}
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <hr />
+          <Button
+            onClick={() => {
+              let save_data = {
+                data_source: data_source.name,
+                query: query.name,
+                name: pipelineName,
+                caption: pipeline,
+                inputs: inputs,
+                wips: wips,
+                outputs: outputs,
+              };
+              const headers = {
+                authorization: "Basic " + token,
+                "Content-Type": "application/json",
+              };
+              const url =
+                "https://docs.ipam.ucla.edu/cocytus/save_pipeline.php";
+              const body = JSON.stringify(save_data);
 
-          console.log(
-            "saving pipline before fetch url=",
-            url,
-            "body=",
-            body,
-            "headers=",
-            headers,
-          );
-          fetch(url, {
-            headers: headers,
-            body: body,
-            mode: "cors",
-            method: "POST",
-          }).then((response) => {
-            response.text().then((text) => {
-              console.log("Saving pipline. Got response: " + text);
-            });
-          });
-          //console.log(JSON.stringify(save_data));
-        }}
-      >
-        Save
-      </Button>
-      <hr />
+              console.log(
+                "saving pipline before fetch url=",
+                url,
+                "body=",
+                body,
+                "headers=",
+                headers,
+              );
+              fetch(url, {
+                headers: headers,
+                body: body,
+                mode: "cors",
+                method: "POST",
+              }).then((response) => {
+                response.text().then((text) => {
+                  console.log("Saving pipline. Got response: " + text);
+                });
+              });
+              //console.log(JSON.stringify(save_data));
+            }}
+          >
+            Save
+          </Button>
+          <hr />
+        </>
+      ) : (
+        <div>Loading data sources...</div>
+      )}
     </div>
   );
 }
