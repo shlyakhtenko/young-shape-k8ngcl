edc79b2a850c61da271015452c79ee69b03abfce
Shlyakhtenko <90537026+shlyakhtenko@users.noreply.github.com>
2024-01-10 03:07:39 +0000

    Update Assignment.js

--- src/assignments/Assignment.js
+++ src/assignments/Assignment.js
@@ -4,29 +4,49 @@ import Select from "react-select";
 export default function Assignments() {
   const [programCode, setProgramCode] = useState(null);
   const [pipeline, setPipeline] = useState(null);
+  const [workshops, setWorkshops] = useState([]);
+  const [pipelines, setPipelines] = useState([]);
+  const [workshopsLoaded, setWorkshopsLoaded] = useState(false);
+  const [pipelinesLoaded, setPipelinesLoaded] = useState(false);
 
-  let workshops = [
-    { label: "GSIWS1", value: "GSIWS1" },
-    { label: "GSIWS2", value: "GSIWS2" },
-    { label: "GSIWS3", value: "GSIWS3" },
-  ];
-
-  let pipelines = [
-    { label: "Collect Titles", value: "talk_titles" },
-    { label: "Confirm Speakers", value: "confirm_speakers" },
-  ];
+  if (!workshopsLoaded) {
+    fetch("https://docs.ipam.ucla.edu/cocytus/get_programs.php").then(
+      (response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setWorkshops(data);
+          setWorkshopsLoaded(true);
+        });
+      },
+    );
+  }
+  if (!pipelinesLoaded) {
+    fetch("https://docs.ipam.ucla.edu/cocytus/get_pipelines.php").then(
+      (response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setPipelines(data);
+          setPipelinesLoaded(true);
+        });
+      },
+    );
+  }
 
   return (
     <div>
       <h1>Assignments</h1>
       <Select
-        options={workshops}
+        options={workshops.map((v) => {
+          return { label: v.programname, value: v.programcode };
+        })}
         onChange={(v) => {
           setProgramCode(v.value);
         }}
       ></Select>
       <Select
-        options={pipelines}
+        options={pipelines.map((v) => {
+          return { label: v.caption, value: v.name };
+        })}
         onChange={(val) => {
           setPipeline(val.value);
         }}
